7908f167256a534e16e81f88f8e7fcd5f7d5c7ed
Akshith <akshithnukala9@gmail.com>
2021-12-14 09:04:49 +0000

    add default version bash script in commented

--- task.sh
+++ task.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#given
+# python3 task.py "$@"
+
 # create var pythonScriptLocation with value of current bash file location
 pythonScriptLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/task.py"
 # run python script with arguments
