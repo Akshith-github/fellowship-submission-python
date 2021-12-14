a55e5f6fe1238a59bafeeff0a12c4d14094992ca
Akshith <akshithnukala9@gmail.com>
2021-12-14 15:01:43 +0530

    changes for windows bat comand task.py to be accesible from other directory

--- task.bat
+++ task.bat
@@ -1,3 +1,14 @@
 @echo off
 
-python3 task.py %1 %2 %3
+@REM python3 task.py %1 %2 %3
+
+@REM get the path to current script in variable batpath and append "task.py"
+set batpath=%~dp0 
+@REM remove the spaces at the end of the path
+set batpath=%batpath:~0,-1%
+@REM add "task.py" to the path
+set pythonfilepath=%batpath%task.py
+@REM echo "python3 %batpath% %1 %2 %3"
+
+
+python3 "%pythonfilepath%" %1 %2 %3
